3adbb01c62e62a1f8af243ed51cca87f0d6078df
EC2 Default User <ec2-user@ip-172-31-17-179.ec2.internal>
2021-09-21 13:46:01 +0000

    Add 各index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,24 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.user_id = current_user.id
     book.save
     redirect_to book_path(book.id)
   end
-  
+
   def show
     @book = Book.new
+    @book = Book.find(params[:id])
+    @user =  @book.user
   end
 
   def index
+    @book = Book.new
   end
 
   def edit
@@ -26,9 +29,9 @@ class BooksController < ApplicationController
 
   def destroy
   end
-  
+
   private
   def book_params
-    params.requier(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,26 @@
 class UsersController < ApplicationController
-  
+
   def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = Book.all
   end
-  
+
   def index
-  end
-  
+    @book = Book.new
+    @users = User.all
+    @user = current_user
+  end 
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
